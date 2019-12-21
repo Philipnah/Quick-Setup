bb109e31af484916859bc7c70f444a847430049c
Philipnah <Philip@han.dk>
2019-12-21 17:42:46 +0100

    added more installers

--- Quick-Setup.py
+++ Quick-Setup.py
@@ -10,6 +10,20 @@ firefox_url = "https://download.mozilla.org/?product=firefox-stub&os=win&lang=en
 firefox_name = "Firefox Installer.exe"
 vsc_url = "https://aka.ms/win32-x64-user-stable"
 vsc_name = "VSCodeUserSetup-x64-1.41.1.exe"
+telegram_url = "https://telegram.org/dl/desktop/win"
+telegram_name = "tsetup.1.8.15.exe"
+tor_url = "https://www.torproject.org/dist/torbrowser/9.0.2/torbrowser-install-win64-9.0.2_en-US.exe"
+tor_name = "torbrowser-install-win64-9.0.2_en-US.exe"
+nordVPN_url = "https://downloads.nordcdn.com/apps/windows/10/NordVPN/latest/NordVPNSetup.exe"
+nordVPN_name = "NordVPNSetup.exe"
+arduino_url = "https://www.arduino.cc/download_handler.php"
+arduino_name = "arduino-1.8.10-windows.exe"
+bitdefender_url = "https://download.bitdefender.com/windows/bp/agent/en-us/bitdefender_online.exe"
+bitdefender_name = "bitdefender_online.exe"
+flux_url = "https://justgetflux.com/dlwin.html"
+flux_name = "flux-setup.exe"
+evernote_url = "https://cdn1.evernote.com/win6/public/Evernote_6.22.3.8816.exe"
+evernote_name = "Evernote_6.22.3.8816.exe"
 
 # Now we set all the variables up in an array matching by url the name
 Installers = [
@@ -20,8 +34,21 @@ Installers = [
      firefox_url,
      firefox_name,
      vsc_url,
-     vsc_name
-
+     vsc_name,
+     telegram_url,
+     telegram_name,
+     tor_url,
+     tor_name,
+     nordVPN_url,
+     nordVPN_name,
+     arduino_url,
+     arduino_name,
+     bitdefender_url,
+     bitdefender_name,
+     flux_url,
+     flux_name,
+     evernote_url,
+     evernote_name
 ]
 
 def getInstallers(URL, installer_name):
@@ -29,16 +56,14 @@ def getInstallers(URL, installer_name):
      
      file_and_location = "installers/" + installer_name
 
-     with open(file_and_location,"wb") as exe: 
+     with open(file_and_location, "wb") as exe: 
           for chunk in r.iter_content(chunk_size=1024): 
           # writing one chunk at a time to pdf file 
                if chunk: 
                     exe.write(chunk)
 
 
-# Amount of installers needed times two
 i = 0
-
 while i < len(Installers):
      # Specify url first then the name
      getInstallers(Installers[i], Installers[i + 1])
